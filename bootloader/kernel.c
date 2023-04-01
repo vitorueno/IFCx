1e12432c8de3f02f10260619f649a5eb80ac410e
vitorueno <vitoruenootto@gmail.com>
2023-04-01 16:34:22 -0300

    feat: adição de funções básicas e testes

--- bootloader/kernel.c
+++ bootloader/kernel.c
@@ -1,10 +1,10 @@
 #include "print.h"
 #include "types.h"
+#include "lib.h"
 
 
 int main() // definindo como externa para o linker solucionar referências
 {
-
     // testando os boleanos
     bool teste = true;
     char *mensagem = "Bem vindo ao IFCX\0";
@@ -13,6 +13,17 @@ int main() // definindo como externa para o linker solucionar referências
         printString(mensagem); 
         printNumAt(strLen(mensagem), 1,1);
     }
+
+
+    char csrc[] = "Teste memcpy";
+    char cdest[100]; 
+    memcpy(cdest, csrc, strlen(csrc) +1);
+    printStringAt(cdest, 2, 1);
+    memset(cdest, 'x', 80);
+    printStringAt(cdest, 3, 1);
+
+    size_t a = 10;
+
     
     return 0; // retornando o controle para kernel_entry (assembly)
 }
---
+++ bootloader/lib.h
@@ -0,0 +1,15 @@
+#ifndef __LIB_H__
+#define __LIB_H__
+
+
+#include "types.h"
+
+extern void *memcpy(void *dest, void *src, size_t count);  // copia bytes de src para dest 
+extern void *memset(void *dest, char val, size_t count); // settar bytes de dest para val
+extern uint16_t *memsetw(uint16_t *dest, uint16_t val, size_t count); // memset versão word (32 bytes)
+extern size_t strlen(const char *str); // retorna o tamanho de uma string
+extern uint8_t inportb (uint16_t _port); // versão C da instrução in 
+extern void outportb (uint16_t _port, uint8_t _data); // versão C da instrução out
+
+
+#endif // __LIB_H__
