cc062fa7b9b6adc316c89a397bbcee8fb7cc1a51
vitorueno <vitoruenootto@gmail.com>
2023-04-18 19:56:34 -0300

    feat: adição das ISRs de exceção (0 a 31)
    adição do código responsável por tratar exceções (interrupções de 0 a 31) com a estratégia "two-stage assembly wrapping"

--- bootloader/isr.c
+++ bootloader/isr.c
@@ -4,264 +4,72 @@
 #include "print.h"
 
 void __attribute((cdecl)) isr0();
-// void __attribute((cdecl)) ISR1();
-// void __attribute((cdecl)) ISR2();
-// void __attribute((cdecl)) ISR3();
-// void __attribute((cdecl)) ISR4();
-// void __attribute((cdecl)) ISR5();
-// void __attribute((cdecl)) ISR6();
-// void __attribute((cdecl)) ISR7();
-// void __attribute((cdecl)) ISR8();
-// void __attribute((cdecl)) ISR9();
-// void __attribute((cdecl)) ISR10();
-// void __attribute((cdecl)) ISR11();
-// void __attribute((cdecl)) ISR12();
-// void __attribute((cdecl)) ISR13();
-// void __attribute((cdecl)) ISR14();
-// void __attribute((cdecl)) ISR15();
-// void __attribute((cdecl)) ISR16();
-// void __attribute((cdecl)) ISR17();
-// void __attribute((cdecl)) ISR18();
-// void __attribute((cdecl)) ISR19();
-// void __attribute((cdecl)) ISR20();
-// void __attribute((cdecl)) ISR21();
-// void __attribute((cdecl)) ISR22();
-// void __attribute((cdecl)) ISR23();
-// void __attribute((cdecl)) ISR24();
-// void __attribute((cdecl)) ISR25();
-// void __attribute((cdecl)) ISR26();
-// void __attribute((cdecl)) ISR27();
-// void __attribute((cdecl)) ISR28();
-// void __attribute((cdecl)) ISR29();
-// void __attribute((cdecl)) ISR30();
-// void __attribute((cdecl)) ISR31();
-// void __attribute((cdecl)) ISR32();
-// void __attribute((cdecl)) ISR33();
-// void __attribute((cdecl)) ISR34();
-// void __attribute((cdecl)) ISR35();
-// void __attribute((cdecl)) ISR36();
-// void __attribute((cdecl)) ISR37();
-// void __attribute((cdecl)) ISR38();
-// void __attribute((cdecl)) ISR39();
-// void __attribute((cdecl)) ISR40();
-// void __attribute((cdecl)) ISR41();
-// void __attribute((cdecl)) ISR42();
-// void __attribute((cdecl)) ISR43();
-// void __attribute((cdecl)) ISR44();
-// void __attribute((cdecl)) ISR45();
-// void __attribute((cdecl)) ISR46();
-// void __attribute((cdecl)) ISR47();
-// void __attribute((cdecl)) ISR48();
-// void __attribute((cdecl)) ISR49();
-// void __attribute((cdecl)) ISR50();
-// void __attribute((cdecl)) ISR51();
-// void __attribute((cdecl)) ISR52();
-// void __attribute((cdecl)) ISR53();
-// void __attribute((cdecl)) ISR54();
-// void __attribute((cdecl)) ISR55();
-// void __attribute((cdecl)) ISR56();
-// void __attribute((cdecl)) ISR57();
-// void __attribute((cdecl)) ISR58();
-// void __attribute((cdecl)) ISR59();
-// void __attribute((cdecl)) ISR60();
-// void __attribute((cdecl)) ISR61();
-// void __attribute((cdecl)) ISR62();
-// void __attribute((cdecl)) ISR63();
-// void __attribute((cdecl)) ISR64();
-// void __attribute((cdecl)) ISR65();
-// void __attribute((cdecl)) ISR66();
-// void __attribute((cdecl)) ISR67();
-// void __attribute((cdecl)) ISR68();
-// void __attribute((cdecl)) ISR69();
-// void __attribute((cdecl)) ISR70();
-// void __attribute((cdecl)) ISR71();
-// void __attribute((cdecl)) ISR72();
-// void __attribute((cdecl)) ISR73();
-// void __attribute((cdecl)) ISR74();
-// void __attribute((cdecl)) ISR75();
-// void __attribute((cdecl)) ISR76();
-// void __attribute((cdecl)) ISR77();
-// void __attribute((cdecl)) ISR78();
-// void __attribute((cdecl)) ISR79();
-// void __attribute((cdecl)) ISR80();
-// void __attribute((cdecl)) ISR81();
-// void __attribute((cdecl)) ISR82();
-// void __attribute((cdecl)) ISR83();
-// void __attribute((cdecl)) ISR84();
-// void __attribute((cdecl)) ISR85();
-// void __attribute((cdecl)) ISR86();
-// void __attribute((cdecl)) ISR87();
-// void __attribute((cdecl)) ISR88();
-// void __attribute((cdecl)) ISR89();
-// void __attribute((cdecl)) ISR90();
-// void __attribute((cdecl)) ISR91();
-// void __attribute((cdecl)) ISR92();
-// void __attribute((cdecl)) ISR93();
-// void __attribute((cdecl)) ISR94();
-// void __attribute((cdecl)) ISR95();
-// void __attribute((cdecl)) ISR96();
-// void __attribute((cdecl)) ISR97();
-// void __attribute((cdecl)) ISR98();
-// void __attribute((cdecl)) ISR99();
-// void __attribute((cdecl)) ISR100();
-// void __attribute((cdecl)) ISR101();
-// void __attribute((cdecl)) ISR102();
-// void __attribute((cdecl)) ISR103();
-// void __attribute((cdecl)) ISR104();
-// void __attribute((cdecl)) ISR105();
-// void __attribute((cdecl)) ISR106();
-// void __attribute((cdecl)) ISR107();
-// void __attribute((cdecl)) ISR108();
-// void __attribute((cdecl)) ISR109();
-// void __attribute((cdecl)) ISR110();
-// void __attribute((cdecl)) ISR111();
-// void __attribute((cdecl)) ISR112();
-// void __attribute((cdecl)) ISR113();
-// void __attribute((cdecl)) ISR114();
-// void __attribute((cdecl)) ISR115();
-// void __attribute((cdecl)) ISR116();
-// void __attribute((cdecl)) ISR117();
-// void __attribute((cdecl)) ISR118();
-// void __attribute((cdecl)) ISR119();
-// void __attribute((cdecl)) ISR120();
-// void __attribute((cdecl)) ISR121();
-// void __attribute((cdecl)) ISR122();
-// void __attribute((cdecl)) ISR123();
-// void __attribute((cdecl)) ISR124();
-// void __attribute((cdecl)) ISR125();
-// void __attribute((cdecl)) ISR126();
-// void __attribute((cdecl)) ISR127();
-// void __attribute((cdecl)) ISR128();
-// void __attribute((cdecl)) ISR129();
-// void __attribute((cdecl)) ISR130();
-// void __attribute((cdecl)) ISR131();
-// void __attribute((cdecl)) ISR132();
-// void __attribute((cdecl)) ISR133();
-// void __attribute((cdecl)) ISR134();
-// void __attribute((cdecl)) ISR135();
-// void __attribute((cdecl)) ISR136();
-// void __attribute((cdecl)) ISR137();
-// void __attribute((cdecl)) ISR138();
-// void __attribute((cdecl)) ISR139();
-// void __attribute((cdecl)) ISR140();
-// void __attribute((cdecl)) ISR141();
-// void __attribute((cdecl)) ISR142();
-// void __attribute((cdecl)) ISR143();
-// void __attribute((cdecl)) ISR144();
-// void __attribute((cdecl)) ISR145();
-// void __attribute((cdecl)) ISR146();
-// void __attribute((cdecl)) ISR147();
-// void __attribute((cdecl)) ISR148();
-// void __attribute((cdecl)) ISR149();
-// void __attribute((cdecl)) ISR150();
-// void __attribute((cdecl)) ISR151();
-// void __attribute((cdecl)) ISR152();
-// void __attribute((cdecl)) ISR153();
-// void __attribute((cdecl)) ISR154();
-// void __attribute((cdecl)) ISR155();
-// void __attribute((cdecl)) ISR156();
-// void __attribute((cdecl)) ISR157();
-// void __attribute((cdecl)) ISR158();
-// void __attribute((cdecl)) ISR159();
-// void __attribute((cdecl)) ISR160();
-// void __attribute((cdecl)) ISR161();
-// void __attribute((cdecl)) ISR162();
-// void __attribute((cdecl)) ISR163();
-// void __attribute((cdecl)) ISR164();
-// void __attribute((cdecl)) ISR165();
-// void __attribute((cdecl)) ISR166();
-// void __attribute((cdecl)) ISR167();
-// void __attribute((cdecl)) ISR168();
-// void __attribute((cdecl)) ISR169();
-// void __attribute((cdecl)) ISR170();
-// void __attribute((cdecl)) ISR171();
-// void __attribute((cdecl)) ISR172();
-// void __attribute((cdecl)) ISR173();
-// void __attribute((cdecl)) ISR174();
-// void __attribute((cdecl)) ISR175();
-// void __attribute((cdecl)) ISR176();
-// void __attribute((cdecl)) ISR177();
-// void __attribute((cdecl)) ISR178();
-// void __attribute((cdecl)) ISR179();
-// void __attribute((cdecl)) ISR180();
-// void __attribute((cdecl)) ISR181();
-// void __attribute((cdecl)) ISR182();
-// void __attribute((cdecl)) ISR183();
-// void __attribute((cdecl)) ISR184();
-// void __attribute((cdecl)) ISR185();
-// void __attribute((cdecl)) ISR186();
-// void __attribute((cdecl)) ISR187();
-// void __attribute((cdecl)) ISR188();
-// void __attribute((cdecl)) ISR189();
-// void __attribute((cdecl)) ISR190();
-// void __attribute((cdecl)) ISR191();
-// void __attribute((cdecl)) ISR192();
-// void __attribute((cdecl)) ISR193();
-// void __attribute((cdecl)) ISR194();
-// void __attribute((cdecl)) ISR195();
-// void __attribute((cdecl)) ISR196();
-// void __attribute((cdecl)) ISR197();
-// void __attribute((cdecl)) ISR198();
-// void __attribute((cdecl)) ISR199();
-// void __attribute((cdecl)) ISR200();
-// void __attribute((cdecl)) ISR201();
-// void __attribute((cdecl)) ISR202();
-// void __attribute((cdecl)) ISR203();
-// void __attribute((cdecl)) ISR204();
-// void __attribute((cdecl)) ISR205();
-// void __attribute((cdecl)) ISR206();
-// void __attribute((cdecl)) ISR207();
-// void __attribute((cdecl)) ISR208();
-// void __attribute((cdecl)) ISR209();
-// void __attribute((cdecl)) ISR210();
-// void __attribute((cdecl)) ISR211();
-// void __attribute((cdecl)) ISR212();
-// void __attribute((cdecl)) ISR213();
-// void __attribute((cdecl)) ISR214();
-// void __attribute((cdecl)) ISR215();
-// void __attribute((cdecl)) ISR216();
-// void __attribute((cdecl)) ISR217();
-// void __attribute((cdecl)) ISR218();
-// void __attribute((cdecl)) ISR219();
-// void __attribute((cdecl)) ISR220();
-// void __attribute((cdecl)) ISR221();
-// void __attribute((cdecl)) ISR222();
-// void __attribute((cdecl)) ISR223();
-// void __attribute((cdecl)) ISR224();
-// void __attribute((cdecl)) ISR225();
-// void __attribute((cdecl)) ISR226();
-// void __attribute((cdecl)) ISR227();
-// void __attribute((cdecl)) ISR228();
-// void __attribute((cdecl)) ISR229();
-// void __attribute((cdecl)) ISR230();
-// void __attribute((cdecl)) ISR231();
-// void __attribute((cdecl)) ISR232();
-// void __attribute((cdecl)) ISR233();
-// void __attribute((cdecl)) ISR234();
-// void __attribute((cdecl)) ISR235();
-// void __attribute((cdecl)) ISR236();
-// void __attribute((cdecl)) ISR237();
-// void __attribute((cdecl)) ISR238();
-// void __attribute((cdecl)) ISR239();
-// void __attribute((cdecl)) ISR240();
-// void __attribute((cdecl)) ISR241();
-// void __attribute((cdecl)) ISR242();
-// void __attribute((cdecl)) ISR243();
-// void __attribute((cdecl)) ISR244();
-// void __attribute((cdecl)) ISR245();
-// void __attribute((cdecl)) ISR246();
-// void __attribute((cdecl)) ISR247();
-// void __attribute((cdecl)) ISR248();
-// void __attribute((cdecl)) ISR249();
-// void __attribute((cdecl)) ISR250();
-// void __attribute((cdecl)) ISR251();
-// void __attribute((cdecl)) ISR252();
-// void __attribute((cdecl)) ISR253();
-// void __attribute((cdecl)) ISR254();
-// void __attribute((cdecl)) ISR255();
+void __attribute((cdecl)) isr1();
+void __attribute((cdecl)) isr2();
+void __attribute((cdecl)) isr3();
+void __attribute((cdecl)) isr4();
+void __attribute((cdecl)) isr5();
+void __attribute((cdecl)) isr6();
+void __attribute((cdecl)) isr7();
+void __attribute((cdecl)) isr8();
+void __attribute((cdecl)) isr9();
+void __attribute((cdecl)) isr10();
+void __attribute((cdecl)) isr11();
+void __attribute((cdecl)) isr12();
+void __attribute((cdecl)) isr13();
+void __attribute((cdecl)) isr14();
+void __attribute((cdecl)) isr15();
+void __attribute((cdecl)) isr16();
+void __attribute((cdecl)) isr17();
+void __attribute((cdecl)) isr18();
+void __attribute((cdecl)) isr19();
+void __attribute((cdecl)) isr20();
+void __attribute((cdecl)) isr21();
+void __attribute((cdecl)) isr22();
+void __attribute((cdecl)) isr23();
+void __attribute((cdecl)) isr24();
+void __attribute((cdecl)) isr25();
+void __attribute((cdecl)) isr26();
+void __attribute((cdecl)) isr27();
+void __attribute((cdecl)) isr28();
+void __attribute((cdecl)) isr29();
+void __attribute((cdecl)) isr30();
+void __attribute((cdecl)) isr31();
+
 
 void initializeISRGates() {
-    setIdtGate(0, isr0, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(0,  isr0, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(1,  isr1, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(2,  isr2, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(3,  isr3, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(4,  isr4, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(5,  isr5, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(6,  isr6, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(7,  isr7, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(8,  isr8, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(9,  isr9, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(10, isr10, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(11, isr11, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(12, isr12, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(13, isr13, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(14, isr14, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(15, isr15, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(16, isr16, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(17, isr17, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(18, isr18, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(19, isr19, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(20, isr20, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(21, isr21, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(22, isr22, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(23, isr23, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(24, isr24, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(25, isr25, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(26, isr26, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(27, isr27, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(28, isr28, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(29, isr29, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(30, isr30, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
+    setIdtGate(33, isr31, GDT_CODE_SEGMENT, IDT_FLAG_RING0 | IDT_FLAG_GATE_32BIT_INT);
 }
 
 ISRHandler isrHandlers[256];
@@ -305,9 +113,60 @@ static const char* const exceptionMessages[] = {
 extern void fim();
 
 void __attribute__((cdecl)) isrHandler(Registers *regs) {
-    printStringAt("Erro", 8, 1);
+    if (regs->interrupt <=32) {
+        // mostrar erro 
+        printStringAt("Erro numero: ", 1, 1);
+        printNumAt(regs->interrupt,1, 1+strLen("Erro numero: "));
+        printStringAt("\"", 1, 17);
+        printStringAt(exceptionMessages[regs->interrupt],1,18);
+        printStringAt("\"", 1, 18+strLen(exceptionMessages[regs->interrupt]));
 
-    fim();
+        // mostrar regs 
+        printStringAt("eax: ", 2, 1);
+        printNumAt(regs->eax, 2, 1 + strLen("eax: "));
+
+        printStringAt("ebx: ", 3, 1);
+        printNumAt(regs->ebx, 3, 1 + strLen("ebx: "));
+
+        printStringAt("ecx: ", 4, 1);
+        printNumAt(regs->ecx, 4, 1 + strLen("ecx: "));
+
+        printStringAt("edx: ", 5, 1);
+        printNumAt(regs->edx, 5, 1 + strLen("edx: "));
+
+        printStringAt("edi: ", 6, 1);
+        printNumAt(regs->edi, 6, 1 + strLen("edi: "));
+
+        printStringAt("esi: ", 7, 1);
+        printNumAt(regs->esi, 7, 1 + strLen("esi: "));
+
+        printStringAt("ebp: ", 8, 1);
+        printNumAt(regs->ebp, 8, 1 + strLen("ebp: "));
+
+        printStringAt("esp: ", 9, 1);
+        printNumAt(regs->esp, 9, 1 + strLen("ebp: "));
+
+        printStringAt("ss: ", 10, 1);
+        printNumAt(regs->ss, 10, 1 + strLen("ss: "));
+
+        printStringAt("ds: ", 11, 1);
+        printNumAt(regs->ds, 11, 1 + strLen("ds: "));
+
+        printStringAt("cs: ", 12, 1);
+        printNumAt(regs->cs, 12, 1 + strLen("cs: "));
+
+        printStringAt("eflags: ", 13, 1);
+        printNumAt(regs->eflags, 13, 1 + strLen("eflags: "));
+
+        printStringAt("eip: ", 14, 1);
+        printNumAt(regs->eip, 14, 1 + strLen("eip: "));
+
+        fim();
+    } else {
+        printStringAt("interrupcao numero: ", 1, 1);
+        printNumAt(regs->interrupt, 1, 1+strLen("interrupcao numero: "));
+
+    }
 }
 
 void initializeISR() {
@@ -318,7 +177,7 @@ void initializeISR() {
 
     disableIdtGate(0x80);
 
-    printStringAt("ISRs carregadas com sucesso", 11, 1);
+    printStringAt("ISRs carregadas", 24, 1);
 }
 
 void isrRegisterHandler(int interruptNumber, ISRHandler handler)
--- bootloader/kernel.c
+++ bootloader/kernel.c
@@ -4,9 +4,7 @@
 #include "idt.h"
 #include "isr.h"
 
-extern void teste(char c);
-
-int main() // definindo como externa para o linker solucionar referências
+int main()
 {
     initializeIDT();
     initializeISR();
@@ -14,23 +12,5 @@ int main() // definindo como externa para o linker solucionar referências
     char *mensagem = "Bem vindo ao IFCX\0";
     printString(mensagem);
 
-    bool isTest = false;
-
-    if (isTest)
-    {
-        teste('A');
-        printNumAt(strLen(mensagem), 1, 1);
-        char csrc[] = "Teste memcpy";
-        char cdest[100];
-        memcpy(cdest, csrc, strlen(csrc) + 1);
-        printStringAt(cdest, 2, 1);
-        memset(cdest, 'x', 80);
-        printStringAt(cdest, 3, 1);
-        size_t a = 10;
-
-        // error handling
-        // printNumAt(10/0, 6,1);
-    }
-
-    return 0; // retornando o controle para kernel_entry (assembly)
+    return 0; // returning control to assembly code
 }
